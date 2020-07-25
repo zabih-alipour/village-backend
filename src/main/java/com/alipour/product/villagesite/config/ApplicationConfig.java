2a790bfb4e20565e3e185d31d77bda62d069c0f4
Alipour <za.alipour.java@gmail.com>
2020-07-26 01:28:21 +0430

    Implement datasource dynamic user&pass

--- src/main/java/com/alipour/product/villagesite/config/ApplicationConfig.java
+++ src/main/java/com/alipour/product/villagesite/config/ApplicationConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.annotation.Order;
 
 import javax.sql.DataSource;
@@ -24,12 +25,13 @@ public class ApplicationConfig {
     @Value("${spring.datasource.password}")
     private String password;
 
-    @Bean("datasource")
+    @Bean(name = "datasource")
+    @Primary
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return DataSourceBuilder.create()
-                .username(System.getenv(username))
-                .password(System.getenv(password))
+                .username(username)
+                .password(password)
                 .build();
     }
 
--- src/main/java/com/alipour/product/villagesite/config/LiquibaseConfig.java
+++ src/main/java/com/alipour/product/villagesite/config/LiquibaseConfig.java
@@ -1,6 +1,8 @@
 package com.alipour.product.villagesite.config;
 
 import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.liquibase.DataSourceClosingSpringLiquibase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -13,13 +15,14 @@ public class LiquibaseConfig {
 
     private final DataSource dataSource;
 
+    @Autowired
     public LiquibaseConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     @Bean
     public SpringLiquibase liquibase(){
-        SpringLiquibase liquibase = new SpringLiquibase();
+        SpringLiquibase liquibase = new DataSourceClosingSpringLiquibase();
         liquibase.setDataSource(dataSource);
         liquibase.setDropFirst(true);
         liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.yaml");
